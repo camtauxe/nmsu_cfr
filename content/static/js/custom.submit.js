88746c2ea7ba97481fa7a41da50fa3142b8e0c5a
eledavi <eledavi7@gmail.com>
2019-03-21 22:37:02 -0600

    Undid previous commit

--- content/static/js/custom.submit.js
+++ content/static/js/custom.submit.js
@@ -18,9 +18,40 @@ function submitCFR() {
 
   insertSuccessMessage();
   insertNewRow();
-  xmlhttp.onreadystatechange = function() {
-  
-  if (this.readyState == 4 && this.status == 200) {
+
+    if (testing == true){
+      dummyArray2 = JSON.parse(dummyArrayJSON);
+      for (i in dummyArray2) {
+        dummy2 = JSON.parse(dummyArray2[i]);
+        txt = "<i>added " + dummy.name1 + " " + dummy.num1 + " " + dummy.num2 + " " + dummy.num3 + " to CFR</i>";
+        x = document.createElement("P");
+        x.innerHTML = txt;
+        document.getElementById("cfrOutput").appendChild(x);
+      }
+    }
+    if (simData == true){
+      dummyArray2 = JSON.parse(dummyArrayJSON);
+      for (i in dummyArray2) {
+        dummy2 = JSON.parse(dummyArray2[i]);
+        var table = document.getElementById("cfrTable");
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        cell1.contentEditable = "true";
+        cell2.contentEditable = "true";
+        cell3.contentEditable = "true";
+        cell4.contentEditable = "true";
+        cell1.innerHTML = dummy2.name1;
+        cell2.innerHTML = dummy2.num1;
+        cell3.innerHTML = dummy2.num2;
+        cell4.innerHTML = dummy2.num3;
+      }
+    }
+    
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
       
     }
   };
