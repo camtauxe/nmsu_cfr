fe3380eea53ac14bbcd2bbcba1264d51394bf7d6
eledavi <eledavi7@gmail.com>
2019-03-21 21:57:26 -0600

    Added functions insertNewRow() and insertSucessMessage()

--- content/static/js/custom.submit.js
+++ content/static/js/custom.submit.js
@@ -16,31 +16,8 @@ function submitCFR() {
   dummyJSON = JSON.stringify(dummy);
   xmlhttp = new XMLHttpRequest();
 
-  if (testing == "true"){
-    dummy2 = JSON.parse(dummyJSON);
-    txt = "<i>added " + dummy[0].name1 + " " + dummy[0].num1 + " " + dummy[0].num2 + " " + dummy[0].num3 + " to CFR</i>";
-    x = document.createElement("P");
-    x.innerHTML = txt;
-    document.getElementById("cfrOutput").appendChild(x);
-  }
-  
-  if (simData == "true"){
-    dummy2 = JSON.parse(dummyJSON);
-    var table = document.getElementById("cfrTable");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    cell1.contentEditable = "true";
-    cell2.contentEditable = "true";
-    cell3.contentEditable = "true";
-    cell4.contentEditable = "true";
-    cell1.innerHTML = dummy2[0].name1;
-    cell2.innerHTML = dummy2[0].num1;
-    cell3.innerHTML = dummy2[0].num2;
-    cell4.innerHTML = dummy2[0].num3;
-  }
+  insertSuccessMessage();
+  insertNewRow();
 
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -48,6 +25,35 @@ function submitCFR() {
   };
 
   xmlhttp.open("POST", "/add_dummy", true);
-  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.setRequestHeader("Content-Type", "text/json");
   xmlhttp.send("x=" + dummyJSON);
+};
+
+function insertNewRow() {
+  dummy2 = JSON.parse(dummyJSON);
+  
+  var table = document.getElementById("cfrTable");
+  var row = table.insertRow(-1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  
+  cell1.contentEditable = "true";
+  cell2.contentEditable = "true";
+  cell3.contentEditable = "true";
+  cell4.contentEditable = "true";
+  
+  cell1.innerHTML = dummy2[0].name1;
+  cell2.innerHTML = dummy2[0].num1;
+  cell3.innerHTML = dummy2[0].num2;
+  cell4.innerHTML = dummy2[0].num3;
+};
+
+function insertSuccessMessage() {
+  dummy2 = JSON.parse(dummyJSON);
+  txt = "<i>added " + dummy[0].name1 + " " + dummy[0].num1 + " " + dummy[0].num2 + " " + dummy[0].num3 + " to CFR</i>";
+  x = document.createElement("P");
+  x.innerHTML = txt;
+  document.getElementById("cfrOutput").appendChild(x);
 }
