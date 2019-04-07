5023dc0ce941cedca0d4e8246a7821865b1b4df4
cameron@ARGON <camerontauxe@gmail.com>
2019-04-07 16:34:00 -0600

    refactor database functions

--- content/utils/request.py
+++ content/utils/request.py
@@ -4,138 +4,100 @@ Functions related to submitting a request
 import json
 from enum import Enum, auto
 from .sql_connection import Transaction
+from .authentication import User
 from . import db_utils
 
-class req_fields(Enum):
-    """
-    Enum representation of course request fields
-    """
-    priority = auto()
-    course = auto()
-    sec = auto()
-    mini_session = auto()
-    online_course = auto()
-    num_students = auto()
-    instructor = auto()
-    banner_id = auto()
-    inst_rank = auto()
-    cost = auto()
-    reason = auto()
+REQ_FIELDS = [
+    'priority',
+    'course',
+    'sec',
+    'mini_session',
+    'online_course',
+    'num_students',
+    'instructor',
+    'banner_id',
+    'inst_rank',
+    'cost',
+    'reason'
+]
 
-class sav_fields(Enum):
+SAL_FIELDS = [
+    'leave_type',
+    'inst_name',
+    'savings',
+    'notes'
+]
+
+INSERT_COURSE = """
+INSERT INTO request
+VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NULL)
+"""
+
+INSERT_SAL = """
+INSERT INTO sal_savings
+VALUES (%s, %s, %s, NULL, %s, %s, %s, %s, %s, NULL)
+"""
+
+def new_cfr_from_courses(user: User, course_list):
     """
-    Enum representation of salary savings fields
+    Add a new cfr revision for the department represented
+    by the given user using the given course data.
+
+    course_list is a list of dicts with all the fields
+    for a course (defined in REQ_FIELDS)
     """
-    leave_type = auto()
-    inst_name = auto()
-    savings = auto()
-    notes = auto()
 
-
-
-#create a new cfr
-#gets the username of user that is currently signed in from wsgi_main
-#gets the semester from the POST request body
-def create_cfr(username, semester):
-    """
-    Insert a new cfr into the cfr_department table
-    """
-    if all(k in semester for k in ['semester']):
-        semester = semester['semester'][0]
-    submitter = username
-    dept_name = db_utils.get_dept(username)
-
-    create_cfr = ("INSERT INTO cfr_department "
-                  "VALUES (%s, %s, YEAR(NOW()), NOW(), NULL, 0, %s)")
-    data_cfr = dept_name + (semester, submitter)
-
-    # execute insert statement to create new cfr
+    rows_inserted = 0
     with Transaction() as cursor:
-        cursor.execute(create_cfr, data_cfr)
-        rows_inserted = cursor.rowcount
-    return rows_inserted
+        db_utils.create_new_revision(cursor, user)
+        new_cfr = db_utils.current_cfr(cursor, user)
 
+        cfr_data = (new_cfr[0], new_cfr[1], new_cfr[2], new_cfr[5])
+        data_ls = []
 
-def add_course(username, data):
-    """
-    Add a course to a cfr
-    -parameter should be a json string containing a list of dictionaries 
-     containing the input information for the course requests
+        for course in course_list:
+            course_data = ()
+            for field in REQ_FIELDS:
+                course_data = course_data + (course[field],)
+            data_ls.append(course_data + cfr_data)
 
-    -can now insert multiple records at a time
-    """
-    #get the data from the current cfr
-    current_cfr = db_utils.current_cfr(username)
-    cfr_data = (current_cfr[0], current_cfr[1], current_cfr[2], current_cfr[5])
-
-    #load the string to a json string containing data in a list of dictionaries 
-    json_data = json.loads(data)
-    data_ls = []
-
-    #loop through each dictionary and parse the values and insert them into tuples
-    for i in range(len(json_data)):
-        data_req = ()
-        request = json_data[i]
-        for j in range(len(request)):
-            data_req = data_req + (request[req_fields(j+1).name], )
-
-        data_req = data_req + cfr_data
-
-        #apend each tuple to a list of tuples
-        data_ls.append(data_req) 
-
-
-    add_req = ("INSERT INTO request "
-               "VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, NULL)")
-      
-
-    with Transaction() as cursor:
-        rows_inserted = 0
+        
         for row in data_ls:
-            cursor.execute(add_req, row)
+            cursor.execute(INSERT_COURSE, row)
             rows_inserted += cursor.rowcount
+
     return rows_inserted
 
-
-def add_sal_savings(username, data):
+def new_cfr_from_sal_savings(user: User, sal_list):
     """
-    Add salary savings to a cfr
+    Add a new cfr revision for the department represented
+    by the given user using the given salary savings data.
 
-    -Parameter is a json string containing a list of dictionaries containing 
-     the input data for the salary savings record.
-
-    -can now insert multiple records at a time
+    sal_list is a list of dicts with all the fields
+    for salary savings (defined in SAL_FIELDS)
     """
-    current_cfr = db_utils.current_cfr(username)
-    cfr_data = (current_cfr[0], current_cfr[1], current_cfr[2], current_cfr[5])
 
-
-    #load the string into a json string and parse the dictionary called 'savings'
-    json_data = json.loads(data)
-    data_ls = []
-
-    #loop through list of dictionaries, extract values and put them into tuples
-    for i in range(len(json_data)):
-        data_sav = ()
-        savings = json_data[i]
-        for j in range(len(savings)):
-            data_sav = data_sav + (savings[sav_fields(j+1).name], )
-
-        data_sav = data_sav + cfr_data
-
-        #create a list of tuples
-        data_ls.append(data_sav)
-    
-
-    add_sav = ("INSERT INTO sal_savings "
-               "VALUES (%s, %s, %s, NULL, %s, %s, %s, %s, %s, NULL)")
-   
+    rows_inserted = 0
     with Transaction() as cursor:
-        rows_inserted = 0
+        db_utils.create_new_revision(cursor, user)
+        new_cfr = db_utils.current_cfr(cursor, user)
+
+        cfr_data = (new_cfr[0], new_cfr[1], new_cfr[2], new_cfr[5])
+        data_ls = []
+
+        for sal in sal_list:
+            sal_data = ()
+            for field in SAL_FIELDS:
+                sal_data = sal_data + (sal[field],)
+            data_ls.append(sal_data + cfr_data)
+
+        
         for row in data_ls:
-            cursor.execute(add_sav, row)
+            cursor.execute(INSERT_SAL, row)
             rows_inserted += cursor.rowcount
+
     return rows_inserted
+
 
     
 
